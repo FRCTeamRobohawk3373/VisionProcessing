8031a5f782d88d3de93c56abed83e628c4d6a144
Robohawks <RoboHawks@10.0.28.28>
2020-01-21 15:54:34 -0500

    json progress

--- Vision/bin/visionCore.py
+++ Vision/bin/visionCore.py
@@ -1,5 +1,15 @@
-from json import loads, dumps
+import json
+
+
+def loadJSON(file='../etc/config.json'):
+    with open(file, 'r') as j:
+        return json.load(j)
 
 
 def getCameraSettings(id):
     return {"camera"}
+
+
+if __name__ == '__main__':
+    config = loadJSON()
+    print(json.dumps(config, sort_keys=True, indent=4))

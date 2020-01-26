c6de9d3621aa499656b3f7ddc07b8b7a07cf8577
FRCTeamRoboHawk3373 <frcteamrobohawk3373@gmail.com>
2020-01-26 16:35:53 -0500

    Panda test

--- Vision/bin/cameraTest.py
+++ Vision/bin/cameraTest.py
@@ -1,13 +1,18 @@
 import cv2
+import time
 
-cam = cv2.VideoCapture(1)
+cam = cv2.VideoCapture(0)
+cam.set(cv2.CAP_PROP_FRAME_WIDTH, 1080)
+cam.set(cv2.CAP_PROP_FRAME_HEIGHT, 720)
 
 while(True):
+    time_start = time.time()
     ret, frame = cam.read()
 
-    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    #gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
 
-    cv2.imshow('frame', gray)
+    print("FPS: ", 1.0 / (time.time() - time_start))
+    cv2.imshow('frame', frame)
     if cv2.waitKey(1) & 0xff == ord('q'):
         break
 

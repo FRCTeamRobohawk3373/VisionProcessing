3f9cc90b61566a46ac0008fd43c07cd6a9ada53b
FRCTeamRoboHawk3373 <frcteamrobohawk3373@gmail.com>
2020-02-04 17:55:39 -0500

    Contour testing

--- Vision/bin/processingtest.py
+++ Vision/bin/processingtest.py
@@ -8,28 +8,38 @@ V = [90, 190]
 lowerb = (H[0], S[0], V[0])
 upperb = (H[1], S[1], V[1])
 
-def drawFrame(frame, m=True):
-    cv2.imshow("Image", frame)
-    if m:
-        hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
-        inrange = cv2.inRange(hsv, lowerb, upperb)
-        mask = cv2.bitwise_and(frame, frame, mask = inrange)
-        cv2.imshow("Mask", mask)
+cam = cv2.VideoCapture("/dev/v4l/by-id/usb-HD_Camera_Manufacturer_USB_2.0_Camera-video-index0")
+cam.set(cv2.CAP_PROP_FRAME_WIDTH, 1920)
+cam.set(cv2.CAP_PROP_FRAME_HEIGHT, 1080)
+
+def drawFrame(frame):
+    cv2.imshow("Frame", frame)
+    hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
+    inrange = cv2.inRange(hsv, lowerb, upperb)
+    mask = cv2.bitwise_and(frame, frame, mask = inrange)
+    cv2.imshow("Mask", mask)
+    image = contours(frame, inrange)
+    cv2.imshow("Contours", image)
+    
+def processing(image):
+    
+
+def contours(frame, binframe):
+    conts, hierarchy = cv2.findContours(binframe, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+    cv2.drawContours(frame, conts, -1, (0, 255, 0), 3)
+    return frame
 
 if __name__ == "__main__":
+    _, frame = cam.read()
+    drawFrame(frame)
+    
     while True:
         key = cv2.waitKey(1)
         if key & 0xFF == ord('q'):
-            print(H, S, V)
             break   
         elif key & 0xFF == ord('.'):
             ret, frame = cam.retrieve()
             drawFrame(frame)
-        # elif key & 0xFF == ord('s'):
-            # savindex += 1
-            # ret, frame = cam.retrieve()
-            # path = os.path.relpath("../imgsav/img_{}.jpg".format(savindex))
-            # ret = cv2.imwrite(path, frame)
 
         cam.grab()
 
